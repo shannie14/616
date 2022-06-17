c69697125e3481c449f6f87f58223849761e4091
Shannon Kendall <105214612+shannie14@users.noreply.github.com>
2022-06-16 21:37:50 -0400

    Activities 1-12

---
+++ 08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -0,0 +1,7 @@
+// Assemble: Create/select DOM elements
+var rootEl = $('#root');
+
+// TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
+rootEl.children('ul').children().css('background-color','white');
+// TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+rootEl:
